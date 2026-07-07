31f53894f8c889d811f3a16372706cac2c452421
Your Name <root@server-673991.nrt.ezb.mybluehost.me>
2026-07-07 10:55:07 +0000

    Fix homepage 500: get compte once with try/except in get_context_data

--- echo/apps/core/views/accueil.py
+++ echo/apps/core/views/accueil.py
@@ -41,6 +41,11 @@ class Accueil(LoginRequiredMixin, PermissionRequiredMixin, TemplateView):
         context = super().get_context_data(**kwargs)
         reinitialiser_ordre_passage(self.request)
 
+        try:
+            compte = self.request.user.profil.compte
+        except:
+            return context
+
         if 'msg' in self.request.GET:
             if self.request.GET['msg'] == 'admission_succes':
                 context['msg'] = _('Patient admis avec succès')
@@ -71,23 +76,11 @@ class Accueil(LoginRequiredMixin, PermissionRequiredMixin, TemplateView):
         jour_min = now.replace(hour=0, minute=0)
         jour_max = now.replace(hour=23, minute=59)
 
-        # rdvs = Rdv.objects.filter(Q(compte=self.request.user.profil.compte) &
-        #                          (Q(debut__day=today.day) & Q(debut__month=today.month) & Q(debut__year=today.year))
-        #                          | (Q(ancien_debut__day=today.day) & Q(ancien_debut__month=today.month) & Q(ancien_debut__year=today.year)))
-
-        # Chercher tous les rdv dont la date de debut ou l'anicenne date de début est dans [aujourd'hui-10, aujourd'hui+10]
-        rdvs = Rdv.objects.filter(Q(compte=self.request.user.profil.compte) &
+        rdvs = Rdv.objects.filter(Q(compte=compte) &
                                   (Q(debut__gte=periode_debut) & Q(debut__lte=periode_fin)
                                    | (Q(ancien_debut__gte=periode_debut) & Q(
-                                              ancien_debut__lte=periode_fin))))\
+                                               ancien_debut__lte=periode_fin))))\
                                     .select_related('patient').select_related('praticien')
-
-        """
-        rdvs_json = serializers.serialize('json', list(rdvs), use_natural_foreign_keys=True,
-                                          fields=(
-                                              'id', 'debut', 'ancien_debut', 'nom', 'nom_naissance', 'prenom', 'telephone', 'ville',
-                                              'praticien', 'statut', 'motif', 'nouveau', 'patient', 'patient_rappele'))
-                                              """
         rdvs_json = RdvSerializer(rdvs, many=True)
         context['rdvs_json'] = json.dumps(rdvs_json.data)
 
@@ -96,7 +89,7 @@ class Accueil(LoginRequiredMixin, PermissionRequiredMixin, TemplateView):
 
         admissions = Admission.objects.filter(date__gte=jour_min,
                                               date__lte=jour_max,
-                                              patient__compte=self.request.user.profil.compte) \
+                                              patient__compte=compte) \
                                         .order_by('ordre') \
                                         .select_related('patient').select_related('patient__adresse')
         admissions_json = AdmissionSerializer(admissions, many=True)
@@ -104,20 +97,20 @@ class Accueil(LoginRequiredMixin, PermissionRequiredMixin, TemplateView):
 
         en_exam_count = Admission.objects.filter(
             date__gte=jour_min, date__lte=jour_max,
-            patient__compte=self.request.user.profil.compte,
+            patient__compte=compte,
             statut='2',
         ).count()
         context['en_exam_count'] = en_exam_count
 
         terminated_admissions = Admission.objects.filter(date__gte=jour_min,
                                                          date__lte=jour_max,
-                                                         patient__compte=self.request.user.profil.compte,
+                                                         patient__compte=compte,
                                                          statut='4') \
                                                    .order_by('ordre') \
                                                    .select_related('patient').select_related('patient__adresse')
         context['terminated_admissions_json'] = json.dumps(AdmissionSerializer(terminated_admissions, many=True).data)
 
-        consultations = Consultation.objects.filter(patient__compte=self.request.user.profil.compte,
+        consultations = Consultation.objects.filter(patient__compte=compte,
                                                     date__gte=periode_debut, date__lte=periode_fin) \
                                             .exclude(patient__admission__statut__in=['2', '4'],
                                                      patient__admission__date__gte=jour_min,
@@ -130,7 +123,7 @@ class Accueil(LoginRequiredMixin, PermissionRequiredMixin, TemplateView):
                                                          use_natural_foreign_keys=True)
         context['motifs_consultation_json'] = motifs_consultation_json
 
-        praticiens = Medecin.objects.filter(compte=self.request.user.profil.compte)
+        praticiens = Medecin.objects.filter(compte=compte)
         context['praticiens'] = praticiens
         context['praticiens_json'] = json.dumps(MedecinSerializer(praticiens, many=True).data)
 
@@ -138,7 +131,7 @@ class Accueil(LoginRequiredMixin, PermissionRequiredMixin, TemplateView):
         context['motifs_rdv'] = motifs_rdvs
         context['motifs_rdv_json'] = json.dumps(MotifRdvSerializer(motifs_rdvs, many=True).data)
 
-        devices = Device.objects.filter(compte=self.request.user.profil.compte)
+        devices = Device.objects.filter(compte=compte)
         context['devices_json'] = json.dumps(DeviceSerializer(devices, many=True).data)
 
         return context
